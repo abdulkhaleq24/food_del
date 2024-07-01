c65e499aba8e5de25d9a2db2ae4c160b31b7e7cd
Abdul Khaleq <abdulkhaleq6008@gmail.com>
2024-07-01 20:59:05 +0600

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order your favorite food here</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit delectus ut debitis veniam dolore hic ipsam aut tempore facere voluptates.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
